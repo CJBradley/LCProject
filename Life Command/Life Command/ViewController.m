cb5ecb944cf2e88d41a255f4d93a2681e6128561
Tiffany Allen <tiffanna@usc.edu>
2015-07-07 11:52:30 -0700

    Added date label

--- Life Command/Life Command/ViewController.m
+++ Life Command/Life Command/ViewController.m
@@ -18,6 +18,7 @@
 @property (strong, nonatomic) IBOutlet UIButton *alarmsButton;
 @property (strong, nonatomic) IBOutlet UITextField *eventTextField;
 @property (strong, nonatomic) IBOutlet UITextField *dateTimeTextField;
+@property (strong, nonatomic) IBOutlet UILabel *dateLabel;
 @end
 
 @implementation ViewController
@@ -28,38 +29,44 @@
     self.taskButton.layer.cornerRadius = 45.0;
     self.taskButton.layer.shadowRadius = 10;
     self.taskButton.layer.shadowOpacity = 0.5;
-    self.taskButton.layer.borderWidth = 1;
+    self.taskButton.layer.borderWidth = 3;
     self.taskButton.layer.borderColor = [UIColor blackColor].CGColor;
     
     self.eventButton.layer.cornerRadius = 45.0;
     self.eventButton.layer.shadowRadius = 10;
     self.eventButton.layer.shadowOpacity = 0.5;
-    self.eventButton.layer.borderWidth = 1;
+    self.eventButton.layer.borderWidth = 3;
     self.eventButton.layer.borderColor = [UIColor blackColor].CGColor;
     
     self.notesButton.layer.cornerRadius = 45.0;
     self.notesButton.layer.shadowRadius = 10;
     self.notesButton.layer.shadowOpacity = 0.5;
-    self.notesButton.layer.borderWidth = 1;
+    self.notesButton.layer.borderWidth = 3;
     self.notesButton.layer.borderColor = [UIColor blackColor].CGColor;
     
     self.calendarButton.layer.cornerRadius = 45.0;
     self.calendarButton.layer.shadowRadius = 10;
     self.calendarButton.layer.shadowOpacity = 0.5;
-    self.calendarButton.layer.borderWidth = 1;
+    self.calendarButton.layer.borderWidth = 3;
     self.calendarButton.layer.borderColor = [UIColor blackColor].CGColor;
     
     self.remindersButton.layer.cornerRadius = 45.0;
     self.remindersButton.layer.shadowRadius = 10;
     self.remindersButton.layer.shadowOpacity = 0.5;
-    self.remindersButton.layer.borderWidth = 1;
+    self.remindersButton.layer.borderWidth = 3;
     self.remindersButton.layer.borderColor = [UIColor blackColor].CGColor;
     
     self.alarmsButton.layer.cornerRadius = 45.0;
     self.alarmsButton.layer.shadowRadius = 10;
     self.alarmsButton.layer.shadowOpacity = 0.5;
-    self.alarmsButton.layer.borderWidth = 1;
+    self.alarmsButton.layer.borderWidth = 3;
     self.alarmsButton.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    NSDate *today = [NSDate date];
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    [dateFormat setDateFormat:@"dd/MM/yyyy"];
+    NSString *dateString = [dateFormat stringFromDate:today];
+    self.dateLabel.text = dateString;
 }
 
 - (void)didReceiveMemoryWarning {
