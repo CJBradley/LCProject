009d2ef5126da7ebc0ebf1568a19a55544825caf
Tiffany Allen <tiffanna@usc.edu>
2015-07-06 20:14:54 -0700

    commit

---
+++ Dummy/Dummy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dummy
+//
+//  Created by Tiffany Allen on 7/6/15.
+//  Copyright (c) 2015 Nicole Allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
